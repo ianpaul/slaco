d59a327cfdc87aaf9593aaaceb1c4fef046d992f
Ian Paul <ian@ianpaul.net>
2016-06-17 16:04:42 +0300

    temporary hold
    a few things not working correctly. Will fix it some day.

--- slaco.php
+++ slaco.php
@@ -17,45 +17,31 @@ if($token != '3vRooEUYQoPmYSuDQ1DJKqgh'){ #replace this with the token from your
   $msg = "The token for the slash command doesn't match. No tacos for you. :dissapointed:";
   die($msg);
   echo $msg;
-}
+} 
 
-$taco_HQ = "https://taco-randomizer.herokuapp.com/random/";
-
-# Set up cURL 
-$feed = curl_init();
-
-# Set up options for cURL 
-# We want to get the value back from our query 
-curl_setopt($feed);
-# Make the call and get the response 
-$json = curl_exec($feed);
-# Close the connection 
-curl_close($feed);
+$unparsed_json = file_get_contents("http://taco-randomizer.herokuapp.com/random/");
 
 # Decode the JSON array sent back by isitup.org
-$response_array = json_decode($json, $assoc = TRUE);
+$json_object = json_decode($unparsed_json, true);
 
-if($json === FALSE){
+print_r($json_object);
+
+
+# This code is not working right now. I'll come back to this project in a little bit and figure out what's going wrong.
+#if($unparsed_json === FALSE){
 
   # The Tacofancy API could not be reached 
-  $badNews = "Ack! Tacofancy HQ is down! No tacos for you. :dissapointed:";  exit();}
+#  $badNews = "Ack! Tacofancy HQ is down! No tacos for you. :dissapointed:";
+# } else {
+# foreach($json_object as $items)
+#{
+# $reply = ":taco: Oh yeah, baby! Here's some taco goodness for you! \n" .$items['seasoning']['name']."";
 
-foreach($response_array as $items)
-{
-  $reply = ":taco: Oh yeah, baby! Here's some taco goodness for you! \n" .$taco['shell']['name']."";
+#}
+#}
 
-}
+#echo $reply;
 
-#Now we put the data back into JSON so that Slack will do its styling magic 
-#on the returned link.
-$payload = json_encode(array(
-  "text" => $reply,
-  "unfurl_media" => "true",
-  "unfurl_links" => "true"
-));
-   
-header('Content-Type: application/json');
-echo $reply;
 
 #That's the end of the road mothersucker!
 ?>
